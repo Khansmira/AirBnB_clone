a70ee5c2dd5e71a60271b75580a5c270a2ae3f51
Khansmira <miriamakanwa@gmail.com>
2023-09-17 09:18:27 +0000

    update

--- models/place.py
+++ models/place.py
@@ -11,7 +11,7 @@ class Place(BaseModel):
         user_id (str): The User id.
         name (str): The name of the place.
         description (str): The description of the place.
-        number_rooms (int): The number of rooms of the place.
+        number_rooms (int): The number of rooms in the place.
         number_bathrooms (int): The number of bathrooms of the place.
         max_guest (int): The maximum number of guests of the place.
         price_by_night (int): The price by night of the place.
